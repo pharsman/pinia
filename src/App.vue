17c8cdfe3379dfba3b3fa58cb7c9ff3dae3cec2e
Pharsman <tengo6531@gmail.com>
2022-11-29 16:50:51 +0400

    project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button>Button</button>
+    <h1>+</h1>
+    <h1>-</h1>
+    <h2>0</h2>
+  </div>
+
+</template>
+<script setup>
+import styles from '@/assets/style.scss'
+import { ref } from 'vue';
+</script>
+
+<style lang="scss">
+
+</style>
